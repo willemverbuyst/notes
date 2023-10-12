42fda48a62b4007ba10c2e015fae63451439f058
Willem Verbuyst <willemverbuyst@gmail.com>
2023-10-12 09:09:32 +0200

    Add types, handle submit

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,29 +1,63 @@
+import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
+import { NoteData, Tag } from "./App";
 
-export function NoteForm() {
+type NoteFormProps = {
+  onSubmit: (data: NoteData) => void;
+};
+
+export function NoteForm({ onSubmit }: NoteFormProps) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    onSubmit({
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: [],
+    });
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Stack gap={4}>
         <Row>
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control required />
+              <Form.Control required ref={titleRef} />
             </Form.Group>
           </Col>
           <Col>
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
-              <CreatableReactSelect isMulti />
+              <CreatableReactSelect
+                isMulti
+                value={selectedTags.map((i) => ({
+                  label: i.label,
+                  value: i.id,
+                }))}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((t) => ({
+                      label: t.label,
+                      id: t.value,
+                    }))
+                  );
+                }}
+              />
             </Form.Group>
           </Col>
         </Row>
         <Row>
           <Form.Group controlId="makrdown">
             <Form.Label>Body</Form.Label>
-            <Form.Control required as="textarea" rows={15} />
+            <Form.Control required as="textarea" rows={15} ref={markdownRef} />
           </Form.Group>
         </Row>
         <Row>
